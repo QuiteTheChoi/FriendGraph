f1205f4cc3233c19adac45d3663d596e30ba4456
QuiteTheChoi <jcaverly92@gmail.com>
2015-04-22 01:45:21 -0400

    Think its working

--- src/friends.java
+++ src/friends.java
@@ -1,6 +1,26 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class friends {
-
+static Scanner stdin = new Scanner(System.in);
+	
+	static char getOption() {
+		System.out.println("\tWhat would you like to do?");
+		System.out.println("(1) List all the students at a particular university");
+		System.out.println("(2) Shortest Intro Chain");
+		System.out.println("(3) Cliques");
+		System.out.println("(4) Connectors");
+		System.out.println("(5) Quit");
+		char response = stdin.next().toLowerCase().charAt(0);
+		while (response != 'p' && response != 'r' && response != 'b' && response != 'd' && response != 'a' && response != 'q') {
+			System.out.print("\tYou must enter one of p, r, b, d, a, or q => ");
+			response = stdin.next().toLowerCase().charAt(0);
+		}
+		return response;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
