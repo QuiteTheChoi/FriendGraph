c52fc31dd0f747f0e85b1241803b8868138439a1
QuiteTheChoi <jcaverly92@gmail.com>
2015-04-24 17:59:56 -0400

    Revert "COMMITING"
    This reverts commit aad829d2bcc8b9586163cbbdbe995e56765a4ae6.

--- src/friends.java
+++ src/friends.java
@@ -24,16 +24,18 @@ static Scanner stdin = new Scanner(System.in);
 	public static void main(String[] args) 
 			throws IOException {
 		
-		System.out.print("Enter HTML file name => ");
-		String htmlFile = stdin.next();
+		System.out.print("Enter Friendship Graph file: ");
+		String graphFile = stdin.next();
 		
-		Graph friends = new Graph();
+		Graph friends = new Graph(graphFile);
 		
 		char option;
 		while ((option = getOption()) != 'q') {
 		
 			System.out.println("Test");
 		
-		}		
+		}
+				
 	}
+
 }
